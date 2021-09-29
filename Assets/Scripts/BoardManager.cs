42f9d5b9acfabc0e2dabb5eecb83e6f1e43d1453
Andika Fathin Azfar <deutandev@pm.me>
2021-09-29 10:19:24 +0700

    Randomize tile

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -66,7 +66,35 @@ public class BoardManager : MonoBehaviour
             {
                 TileController newTile = Instantiate(tilePrefab, new Vector2(startPosition.x + ((tileSize.x + offsetTile.x) * x), startPosition.y + ((tileSize.y + offsetTile.y) * y)), tilePrefab.transform.rotation, transform).GetComponent<TileController>();
                 tiles[x, y] = newTile;
+                
+                 // get no tile id
+                List<int> possibleId = GetStartingPossibleIdList(x, y);
+                int newId = possibleId[Random.Range(0, possibleId.Count)];
+
+                newTile.ChangeId(newId, x, y);
             }
         }
+    }
+    
+    private List<int> GetStartingPossibleIdList(int x, int y)
+    {
+        List<int> possibleId = new List<int>();
+
+        for (int i = 0; i < tileTypes.Count; i++)
+        {
+            possibleId.Add(i);
+        }
+
+        if (x > 1 && tiles[x - 1, y].id == tiles[x - 2, y].id)
+        {
+            possibleId.Remove(tiles[x - 1, y].id);
+        }
+
+        if (y > 1 && tiles[x, y - 1].id == tiles[x, y - 2].id)
+        {
+            possibleId.Remove(tiles[x, y - 1].id);
+        }
+
+        return possibleId;
     }
 }
